797dc08e46f19f63114288e3fb962ca36becf91e
unknown <Your GitHub Email>
2024-04-24 18:27:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import Image "";
+import "./App.css";
+import Navbar from './Components/Navbar/Navbar';
+
+
+const App = () => {
+  return (
+    <div className='container'>
+    <Navbar />
+    </div>
+  )
+};
+
+export default App;
